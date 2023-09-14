3c1c3d6aa011f9aaf2637b582541e5f746b1b851
darker <darkerros@yandex.ru>
2023-09-15 01:20:09 +0300

    feat: create base layout and create main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import PostsStore from "../stores/posts-store.ts";
+
+  import AddPostDorm from "../components/AddPostForm.svelte";
+  import PostCardList from "../components/PostCardList.svelte";
+
+  import type { PostResource } from "../types/post-resource.ts";
+
+  const handleAddPost = (post: PostResource) => {
+    PostsStore.update(posts => {
+      posts.push(post)
+      return posts
+    })
+  }
+</script>
+
+<section class="">
+    <div class="main-page__content">
+        <AddPostDorm onSubmit={handleAddPost}/>
+        <PostCardList/>
+    </div>
+</section>
+
+<style lang="scss">
+  .main-page {
+    &__content {
+      gap: 10px;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+    }
+  }
+</style>
