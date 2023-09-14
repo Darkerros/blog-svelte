47bcdd7cfe2c528015556ba6c3f6c62b7bcc2944
darker <darkerros@yandex.ru>
2023-09-15 01:21:44 +0300

    feat: create portal component for modal (similar react portal)

---
+++ src/components/Portal.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let modalContainer = null;
+  let portalRef = null;
+
+  onMount(() => {
+    modalContainer = document.querySelector('#modal');
+    modalContainer.appendChild(portalRef)
+
+    return () => {
+      if (document) {
+        document.body.removeChild(portalRef)
+      }
+    }
+  })
+</script>
+
+<div bind:this={portalRef}>
+    <slot></slot>
+</div>
+
+<style>
+
+</style>
